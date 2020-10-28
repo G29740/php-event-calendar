49f7479f98f3acbf2e94973f3cd402df4e36fb50
G29740 <48676469+G29740@users.noreply.github.com>
2020-10-28 20:30:34 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
    <head>
-      <!-- meta character set -->
       <meta charset="utf-8">
-      <!-- Always force latest IE rendering engine or request Chrome Frame -->
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <title>Calendar test page</title>
-      <!-- Meta Description -->
       <meta name="description" content="PHP Calendar">
       <meta name="keywords" content="calendar, event, PHP, HMTL, CSS, JS, ajax">
       <meta name="author" content="Leye Jin">
-      <!-- Mobile Specific Meta -->
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <!-- ===================== CSS ========================== -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
       <style>
          h1 {
@@ -87,7 +82,7 @@
          <div class="container">
             <div class="row">
                <div class="col-12">
-                  <div id="calendar-html-output">
+                  <div id="calendar-output">
                      <?php
                         include "calendar.php";
                         echo getCalendar($con,date("m",time()),date("Y",time()));
@@ -98,39 +93,8 @@
          </div>
       </main>
       
-      <!-- JavaScript libraries -->
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-      <script>
-      $(document).ready(function() {
-         
-         //attach click event on previous and next month buttons
-         $(document).on("click", '.prev', function(event) { 
-            var month =  $(this).data("prev-month");
-            var year =  $(this).data("prev-year");
-            getCalendar(month,year);
-         });
-         $(document).on("click", '.next', function(event) { 
-            var month =  $(this).data("next-month");
-            var year =  $(this).data("next-year");
-            getCalendar(month,year);
-         });
-      });
-
-      //ajax call to update the calendar
-      function getCalendar(month,year){
-         $.ajax({
-            url: "ajax.php",
-            type: "POST",
-            data:'month='+month+'&year='+year,
-            complete: function(data){
-               $("#calendar-html-output").html(data.responseText);
-            },
-            error: function(xhr,status,error){
-               //handle ajax error here
-            }
-         });
-      }
-      </script>
+      <script src="calendar.js"></script>
 	</body>
 </html>
